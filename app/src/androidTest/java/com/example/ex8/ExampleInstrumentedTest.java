20528696f0f47719076cf1c16e1b3703e8872b59
Sharon <camel2469@gmail.com>
2022-05-15 22:00:08 +0300

    Add some comments

--- app/src/androidTest/java/com/example/ex8/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ex8/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.ex7", appContext.getPackageName());
+        assertEquals("com.example.ex8", appContext.getPackageName());
     }
 }
